1405a8e5290c607cdc3633b9855b24a26b49284d
kostya <Email>
2026-05-29 10:26:04 +0300

    Add max_digit function

--- numbers_lib.py
+++ numbers_lib.py
@@ -2,3 +2,5 @@ def digit_sum(number: int) -> int:
     return sum(int(d) for d in str(abs(number)))
 def count_digits(number):
     return len(str(number))
+def max_digits(number):
+    return max(int(d) for d in str(abs(number)))
